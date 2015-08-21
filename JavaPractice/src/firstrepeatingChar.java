7e3c4cfe031d4b4dc3ca49606c8b7abcd0a2ef6c
lud200 <dludaya09@outlook.com>
2015-08-20 22:52:56 -0400

    new

--- JavaPractice/src/firstrepeatingChar.java
+++ JavaPractice/src/firstrepeatingChar.java
@@ -3,10 +3,11 @@ public class firstrepeatingChar {
 	public static void main(String[] args){
 		char[] ch = {'a', 'b', 'c', 'd', 'b', 'c'};
 		int size = ch.length;
-		char[] temp = new char[size];
+		String s = "";
+		int count = 0;
 		for(int i=0;i<size-1;i++){
+			s=s+ch[i];
 			
-		}
-		
+		}		
 	}
 }
